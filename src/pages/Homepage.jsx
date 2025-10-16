31108daec097dde7bd0b7bd21e539299e09ce9e9
Jovan <jowwanv@gmail.com>
2025-10-16 15:53:51 +0200

    visual scroll change

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,6 +2,7 @@ import HeroButtons from "../components/HeroButtons";
 import HeroText from "../components/HeroText";
 
 function Homepage() {
+  window.scrollTo(0, 0);
   return (
     <div className="flex-col flex w-full bg-gradient-to-b from-slate-800 to-sky-900 px-5 py-24">
       <HeroText />
